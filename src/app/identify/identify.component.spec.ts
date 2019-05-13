69826103f8dc545b27cb67ec34ed1653ae227dee
azeghost <rikispanish123@gmail.com>
2019-05-13 11:27:47 +0200

    further extending Unit tests (#12)

--- src/app/identify/identify.component.spec.ts
+++ src/app/identify/identify.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IdentifyComponent } from './identify.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {readFile} from 'fs';
 
 describe('IdentifyComponent', () => {
   let component: IdentifyComponent;
@@ -25,6 +26,22 @@ describe('IdentifyComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should handle Url Input', () => {
+    async(() => {
+      component.handleUrlInput("https://res.cloudinary.com/demo/image/upload/w_200/lady.jpg"
+        ,"712cd6e9-6d1e-4d46-8279-6e5b753dd2df");
+      expect(component.matchId).toContain("712cd6e9-6d1e-4d46-8279-6e5b753dd2df");
+    });
+  });
+
+  it('should get Preview', () => {
+    async(() => {
+      let file: File;
+      component.getPreview(file);
+      expect(component.url).toContain(file.name);
+    });
   });
 
   it('should render title in a h1 tag', () => {
